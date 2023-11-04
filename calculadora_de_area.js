886c67aab9e4d4734c8d1e14a9f46b9be67d6f0e
devgabsao <126030438+devgabsao@users.noreply.github.com>
2023-11-04 12:32:01 -0300

    Exercício Calculador a de Área JavaScript

---
+++ calculadora_de_area.js
@@ -0,0 +1,9 @@
+let base = prompt("Bem-vindo a calculadora de área, digite o valor da base: ");
+
+let altura = prompt("Digite o valor da altura: ");
+
+function calculaArea(base, area) {
+
+   return ("O Resultado é: " + base * altura + "m²"); }
+
+alert (calculaArea(base, area))
